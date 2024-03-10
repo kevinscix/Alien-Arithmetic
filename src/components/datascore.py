f392fcb315e4d3b88da46e7774d0779d6254f610
Andy Sit <andysit173@gmail.com>
2024-03-10 17:13:10 -0400

    skeleton init

--- src/components/datascore.py
+++ src/components/datascore.py
@@ -1,1 +1,31 @@
 #AndyD
+from pydantic import BaseModel
+from typing import Dict
+
+#represents a single data model for a player to be filled out per user
+#might be user to call this the user model
+
+class DataScore(BaseModel):
+  _name : str
+  _highScore : int
+  _questionsCompleted : int
+  _incorrectAmt : int
+  _correctAmt : int
+  _overallGrade : int
+  _loggedIn : bool
+
+
+  def setName(self, name: str) -> None:
+    self._name = name
+
+  def setHighScore(self, score: int) -> None:
+    self._highScore = score
+
+  def setQuestionsCompleted(self, questions) -> None:
+    self._questionsCompleted = questions
+
+  # # Do we need to create these each time?
+  # def getPlayer(self, player: str) -> None:
+
+if __name__ == "__main__":
+  print("Test cases for DataScore model")
--- src/components/scoreboard.py
+++ src/components/scoreboard.py
@@ -1,1 +1,40 @@
-#emily
+#Emily & Andy
+from datascore import DataScore
+from pydantic import BaseModel
+from typing import Dict
+
+#object to get all the data in csv file and logic
+
+class Scoreboard(BaseModel):
+  #total players
+  numberPlayer : int
+  userType : int
+  Board : list[DataScore]
+
+  #current player
+  score : int
+  currentPlayer : DataScore
+
+  #returns 0 if user is true or 1 if user is false
+  def isPlayer(self):
+    if self.userType == 1:
+      return True
+
+  def isInstructor(self):
+    if self.userType == 0:
+      return True
+
+  def showScores(self):
+    print(self.Board)
+
+  def getPlayer(self, name : str) -> DataScore:
+    pass
+
+  #this should just store the current player store and data.
+  def storeScore(self, player : DataScore) -> None:
+    pass
+
+
+
+
+

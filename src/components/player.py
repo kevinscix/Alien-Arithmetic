08888235878575cf40c94f875852ee75e8abeab0
Kevin Wu <kwu347@uwo.ca>
2024-03-11 16:30:15 -0400

    skeleton innit

--- src/components/player.py
+++ src/components/player.py
@@ -1,1 +1,20 @@
 #Kevin
+class Player:
+    def __init__(self):
+        self.health = 0
+        self.points = 0
+
+    def damage(self):
+        pass
+
+    def getHealth(self):
+        pass
+
+    def addPoints(self, points):
+        pass
+
+    def setPoints(self, points):
+       pass
+
+    def move(self):
+        pass

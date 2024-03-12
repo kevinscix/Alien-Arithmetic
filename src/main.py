054f7185aaf3dcf1916169c6df253e20becd61a2
Kevin Wu <kwu347@uwo.ca>
2024-03-12 12:57:53 -0400

    set up initial game loop and added mouse tracking and keyboard movement skeleton

---
+++ src/main.py
@@ -0,0 +1,63 @@
+#pygame game loop template from pydocs
+import pygame
+import sys
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("black")
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+
+    """
+    make sure to implement movement restriction to specific area of the screen later on
+    """
+    keys = pygame.key.get_pressed()
+    # WASD key movement
+    if keys[pygame.K_w]:
+        player_pos.y -= 300 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 300 * dt
+    # arrow key movement
+    if keys[pygame.K_UP]:
+        player_pos.y -= 300 * dt  
+    if keys[pygame.K_DOWN]:
+        player_pos.y += 300 * dt
+    if keys[pygame.K_LEFT]:
+        player_pos.x -= 300 * dt
+    if keys[pygame.K_RIGHT]:
+        player_pos.x += 300 * dt
+
+    # mouse movement
+    if pygame.mouse.get_focused():
+        player_pos.x, player_pos.y = pygame.mouse.get_pos()
+
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+pygame.quit()

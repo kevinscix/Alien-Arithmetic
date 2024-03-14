0cc5614ea7c984337d973719437514056026e91b
Andy Jun-Lok Sit <asit5@uwo.ca>
2024-03-14 15:20:02 -0400

    save commit

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #pygame game loop template from pydocs
 import pygame
-from components import *
+from components.player import Player
 
 # pygame setup
 pygame.init()
@@ -10,6 +10,7 @@ running = True
 dt = 0
 
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player = Player()
 
 #border for sprite ie like out of bounds since sprite can go out of screen
 #changes to the movement needs to be made
@@ -32,28 +33,28 @@ while running:
     keys = pygame.key.get_pressed()
     # WASD key movement
     if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
+        player_pos.y -= player.getSpeed() * dt
     if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
+        player_pos.y += player.getSpeed() * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        player_pos.x -= player.getSpeed() * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+        player_pos.x += player.getSpeed() * dt
     # arrow key movement
     if keys[pygame.K_UP]:
-        player_pos.y -= 300 * dt  
+        player_pos.y -= player.getSpeed() * dt  
     if keys[pygame.K_DOWN]:
-        player_pos.y += 300 * dt
+        player_pos.y += player.getSpeed() * dt
     if keys[pygame.K_LEFT]:
-        player_pos.x -= 300 * dt
+        player_pos.x -= player.getSpeed() * dt
     if keys[pygame.K_RIGHT]:
-        player_pos.x += 300 * dt
+        player_pos.x += player.getSpeed() * dt
 
     # mouse movement  
-    if pygame.mouse.get_focused():
-        mouseMovement = pygame.mouse.get_rel()
-        player_pos.x += mouseMovement[0]
-        player_pos.y += mouseMovement[1]
+    # if pygame.mouse.get_focused():
+    #     mouseMovement = pygame.mouse.get_rel()
+    #     player_pos.x += mouseMovement[0]
+    #     player_pos.y += mouseMovement[1]
 
     # flip() the display to put your work on screen
     pygame.display.flip()

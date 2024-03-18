1c2a858454588d81e3ac9b33f93739f864976749
jbarke43 <jbarke43@uwo.ca>
2024-03-17 20:09:52 -0400

    added comments to prototype

--- src/components/asteroidVisual.py
+++ src/components/asteroidVisual.py
@@ -6,20 +6,28 @@ class asteroidVisual(asteroid):
     size : int
     yPos : int = 100      # will not be 100, should be the top of screen
 
+    # constructor
     def __init__(self, angle, size, xPos) -> None:
         self.angle = angle
         self.size = size
         self.xPos = xPos
 
+    # moves the asteroid down by the speed
+    # somewhere there will need to be a loop that calls this until the question is answered
     def move(speed) -> None:
         yPos = yPos - speed
 
+    # returns the angle that the asteroid sprite will be displayed at
+    # this is so each asteroid will look different
+    # there will need to be a random generator for this value where the object is created
     def getAngle(self) -> int:
         return self.angle
     
+    # returns size value of asteroid sprite
     def getSize(self) -> int:
         return self.size
     
+    # returns the position of the asteroid in the for [x, y]
     def getPos(self, yPos) -> list:
         position = [self.xPos, yPos]
         return position

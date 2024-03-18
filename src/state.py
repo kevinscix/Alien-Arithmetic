fbf41031003c2e0bf7683f5ffef4c38ac85f9a44
Andy Jun-Lok Sit <asit5@uwo.ca>
2024-03-18 12:33:44 -0400

    save state with examples

--- src/state.py
+++ src/state.py
@@ -1,7 +1,6 @@
 from pydantic import BaseModel
 import json
 
-
 #should handle the state of the saves
 #handle the interface, player?
 class State():
@@ -27,7 +26,7 @@ class State():
 
 #this is the actual contents of the basemodel of the content we want
 #handles the level, access level, and maybe logged in?
-class SaveState(BaseModel):
+class SaveModel(BaseModel):
     name : str
     score : int
     level : list[int]
@@ -49,9 +48,30 @@ class SaveState(BaseModel):
     def setQuestionsCompleted(self, questions) -> None:
       self.questionsCompleted = questions
 
+
+class SaveState(SaveModel, State):
+  def __str__(self) -> str:
+     return "{} with a score of {}.".format(self.name, self.score)
+  pass
+
+
+
 if __name__ == "__main__":
   print("Test cases for DataScore model")
   state = State()
+
+
+  AndyPlayer = SaveState(
+    name="Andy",
+    score=40,
+    level=[2,1,3],
+    highScore=50,
+    questionsCompleted=3,
+    incorrectAmt=1,
+    correctAmt=10,
+    overallGrade=50,
+    loggedIn=True
+  )
 
   #empty player/user data
   EmptyPlayer = SaveState(
@@ -67,10 +87,24 @@ if __name__ == "__main__":
     )
 
 
+
+  EmptyPlayer.save_settings(AndyPlayer.model_dump_json(), "AndyPlayer.txt")
+  EmptyPlayer.save_settings(EmptyPlayer.model_dump_json(), "EmptyPlayer.txt")
+  data = EmptyPlayer.load_settings("EmptyPlayer.txt")
+  andy = EmptyPlayer.load_settings("AndyPlayer.txt")
+  if data:
+    Player : SaveState = EmptyPlayer.model_validate_json(data)
+    AndyPlaye : SaveState = EmptyPlayer.model_validate_json(andy)
+    print(Player)
+    print(AndyPlayer)
+
+
+
+  # print(Player.model_dump_json() , Player.name)
   #testing the load function
-  jsonD = state.load_settings("Andy.txt")
-  Player : SaveState = EmptyPlayer.model_validate_json(jsonD)
-  print(Player.model_dump_json() , Player.name)
+  # jsonD = state.load_settings("Andy.txt")
+  # Player : SaveState = EmptyPlayer.model_validate_json(jsonD)
+  # print(Player.model_dump_json() , Player.name)
 
 
 

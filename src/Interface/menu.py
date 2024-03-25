a2c0adc55020e7f558e04d9b368f8ca9cff030f5
Kevin Wu <kwu347@uwo.ca>
2024-03-25 15:22:01 -0400

    moved code inside def

--- src/Interface/menu.py
+++ src/Interface/menu.py
@@ -21,25 +21,7 @@ from Interface.leaderboard import LeaderboardState
 from Interface.game import GameState
 currentPath = os.path.dirname(__file__)  
 
-menuImagePath = os.path.join(currentPath, "..", "components", "Images", "menuPage.png")
-startImagePath = os.path.join(currentPath, "..", "components", "Images", "startButton.png")
-loadImagePath = os.path.join(currentPath, "..", "components", "Images", "loadButton.png")
-tutorialImagePath = os.path.join(currentPath, "..", "components", "Images", "tutorialButton.png")
-highscoresImagePath = os.path.join(currentPath, "..", "components", "Images", "leaderboardButton.png")
-quitImagePath = os.path.join(currentPath, "..", "components", "Images", "exitButton.png")
 
-menuImage = pygame.image.load(os.path.normpath(menuImagePath))
-menuImage= pygame.transform.scale(menuImage, (800, 600))
-start_image = pygame.image.load(os.path.normpath(startImagePath))
-start_image = pygame.transform.scale(start_image, (200, 75))
-load_image = pygame.image.load(os.path.normpath(loadImagePath))
-load_image = pygame.transform.scale(load_image, (200, 75))
-tutorial_image = pygame.image.load(os.path.normpath(tutorialImagePath))
-tutorial_image = pygame.transform.scale(tutorial_image, (75, 75))
-highscores_image = pygame.image.load(os.path.normpath(highscoresImagePath))
-highscores_image = pygame.transform.scale(highscores_image, (200, 75))
-quit_image = pygame.image.load(os.path.normpath(quitImagePath))
-quit_image = pygame.transform.scale(quit_image, (200, 75))
 
 
     
@@ -48,9 +30,28 @@ class MenuState(State):
         super().__init__(engine)
         #UI
         self.ui = Group()  # Create a group to hold all the ui elements. This is filled with the ui elements below thanks to the ui_group parameter
-        
-
         self.user : SaveModel = None
+
+        self.menuImagePath = os.path.join(currentPath, "..", "components", "Images", "menuPage.png")
+        startImagePath = os.path.join(currentPath, "..", "components", "Images", "startButton.png")
+        loadImagePath = os.path.join(currentPath, "..", "components", "Images", "loadButton.png")
+        tutorialImagePath = os.path.join(currentPath, "..", "components", "Images", "tutorialButton.png")
+        highscoresImagePath = os.path.join(currentPath, "..", "components", "Images", "leaderboardButton.png")
+        quitImagePath = os.path.join(currentPath, "..", "components", "Images", "exitButton.png")
+
+        self.menuImage = pygame.image.load(os.path.normpath(self.menuImagePath))
+        self.menuImage= pygame.transform.scale(self.menuImage, (800, 600))
+        start_image = pygame.image.load(os.path.normpath(startImagePath))
+        start_image = pygame.transform.scale(start_image, (200, 75))
+        load_image = pygame.image.load(os.path.normpath(loadImagePath))
+        load_image = pygame.transform.scale(load_image, (200, 75))
+        tutorial_image = pygame.image.load(os.path.normpath(tutorialImagePath))
+        tutorial_image = pygame.transform.scale(tutorial_image, (75, 75))
+        highscores_image = pygame.image.load(os.path.normpath(highscoresImagePath))
+        highscores_image = pygame.transform.scale(highscores_image, (200, 75))
+        quit_image = pygame.image.load(os.path.normpath(quitImagePath))
+        quit_image = pygame.transform.scale(quit_image, (200, 75))
+
 
         # Start button
         self.btn_start = button.ButtonPngIcon(
@@ -111,7 +112,7 @@ class MenuState(State):
         #so i think this is fine. I wont delete the gameUI yet cause it might be useful for later
     def on_draw(self, surface):
         #change to real background?
-        surface.blit(menuImage, (0, 0))
+        surface.blit(self.menuImage, (0, 0))
 
         self.btn_start.draw(surface, 300, 150)
         self.btn_load.draw(surface, 300, 350)

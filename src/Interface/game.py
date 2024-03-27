80301bc65b60a85521bff26f57af5070c69f2323
Andy Sit <asit5@uwo.ca>
2024-03-26 21:16:20 -0400

    asteroid and questions added to game state

--- src/Interface/game.py
+++ src/Interface/game.py
@@ -25,26 +25,28 @@ class GameState(State):
         #UI
         self.ui = Group()  # Create a group to hold all the ui elements. This is filled with the ui elements below thanks to the ui_group parameter
         WIDTH, HEIGHT = 800, 600
-    
+
         #make this into a utils function?
         currentPath = os.path.dirname(__file__)  # __file__ is the path to the current script
         gameImagePath = os.path.join(currentPath, "..", "components", "Images", "gamePlay1.png")
-        shotImagePlath = os.path.join(current_dir, "..", "components", "Images", "red_bullet.png")
+        shotImagePath = os.path.join(current_dir, "..", "assets", "visuals", "projectiles", "red projectile icon.png")
+
         self.gamePlay1Image = pygame.image.load(os.path.normpath(gameImagePath))
         #we need someone to load in the bullets
-        self.shotImage = pygame.image.load(os.path.normpath(shotImagePlath))
+        self.shotImage = pygame.image.load(os.path.normpath(shotImagePath))
+        self.shotImage = pygame.transform.scale(self.shotImage, (50, 50))
         self.gamePlay1Image = pygame.transform.scale(self.gamePlay1Image, (WIDTH, HEIGHT))
         self.user : SaveModel = None
         self.border = pygame.rect.Rect(10, 10, 600, 20)
-       
+
        # Start button
         self.btn_puase = button.ButtonText(
-            "PAUSE", 
-            self.pause_callback, 
+            "PAUSE",
+            self.pause_callback,
             rect_color=(85, 145, 92),  # Green color
-            fixed_width=200, 
-            border_radius=10, 
-            text_align="center", 
+            fixed_width=200,
+            border_radius=10,
+            text_align="center",
             ui_group=self.ui
         )
 
@@ -54,22 +56,22 @@ class GameState(State):
         self.player_speed = 5
         self.bullet_radius = 5
 
-        #needs a better way to do the width and height 
-            # a big issue is how we pass the surface around this is causing problems 
+        #needs a better way to do the width and height
+            # a big issue is how we pass the surface around this is causing problems
             # think of a work around
         self.player_pos = [WIDTH // 2, ((HEIGHT / 4) * 3 + 20)]
-       
+
         self.asteroidMaster = Asteroid(1)
         self.asteroidMaster.generateAsteroids()
 
     #only button that exsit on the screen
     def pause_callback(self):
         pass
-        
+
     #Game specific functions
     def create_shot(self, player_pos):
         #limits the number of bullets on screen to one
-        if not (len(self.shots) > 0):   
+        if not (len(self.shots) > 0):
             bullet_pos = [player_pos[0], player_pos[1]]
             return {
                 'surface' :  self.shotImage,
@@ -79,9 +81,9 @@ class GameState(State):
             }
 
     def move_shot(self):
-        for shot in self.shots: 
+        for shot in self.shots:
             shot['position'][1] -= shot['speed']
-        
+
 
     def remove_shot(self):
         for shot in self.shots:
@@ -95,48 +97,56 @@ class GameState(State):
                         obj['surface'].get_height())
 
     def shot_collided(self):
-        shot_rect = self.get_rect(self.shots[0])
-        for asteroid in self.asteroidMaster.asteroidArr:
-            if shot_rect.colliderect(self.get_rect(asteroid)):
-                #write logic about point gain and lost for health if correct or not
-                if asteroid['correct']:
-                    #gain health gain store
-                    #how should we draw the health bar onto the screen
-                    #remove asteroids and start new level 
-                    self.newRound()
-                    pass
-                else:
-                    #loss health
-                    pass
-                return True
-        return False
+        try:
+            shot_rect = self.get_rect(self.shots[0])
+            for asteroid in self.asteroidMaster.asteroidArr:
+                if shot_rect.colliderect(self.get_rect(asteroid)):
+                    #write logic about point gain and lost for health if correct or not
+                    if asteroid['correct']:
+                        #gain health gain store
+                        #how should we draw the health bar onto the screen
+                        #remove asteroids and start new level
+                        self.newRound()
+                        pass
+                    else:
+                        #loss health
+                        pass
+                    return True
+            return False
+        except:
+            pass
+
+
 
     def newRound(self):
         self.asteroidMaster.asteroidArr = []
         self.asteroidMaster.generateAsteroids()
-
-
 
     def on_draw(self, surface):
         #pops the screen up
         #problem is the the surface is only passed on surface we can either hard code the width or heights to reduce calling this function
         #over and over again. for the sake of prototyping ill leave it here cause ill figure it out later
         surface.blit(self.gamePlay1Image, (0, 0))
-        
+
         #moves the bullets down the screen
         self.move_shot()
         self.remove_shot()
         self.asteroidMaster.move_asteroids()
 
         for shot in self.shots:
-            pygame.draw.circle(surface, "black", shot['position'], shot['radius'])
+            surface.blit(shot['surface'], shot['position'])
 
+            # pygame.draw.circle(surface, "black", shot['position'], shot['radius'])
         for asteroid in self.asteroidMaster.asteroidArr:
-            pygame.draw.circle(surface, "pink", asteroid['position'], 50)
+            surface.blit(asteroid['surface'], asteroid['position'])
+            surface.blit(asteroid['number_surface'], asteroid['position'])
 
-        if self.shot_collided():
-            print("hit!")
-        
+            # pygame.draw.circle(surface, "pink", asteroid['position'], 50)
+
+        self.shot_collided()
+
+        surface.blit(self.asteroidMaster.question_surface, [500, 300])
+
         pygame.draw.circle(surface, "red", self.player_pos, self.player_radius)
         pygame.draw.rect(surface, "red", self.border)
         pygame.display.flip()
--- src/components/asteroid.py
+++ src/components/asteroid.py
@@ -1,44 +1,51 @@
 #Jackson
 import os
-import sys
 import random
 import pygame
 
-
 class Asteroid():
     # Class-level attributes
-    maxAsteroid : int = 50
-    minAsteroid : int = 1
-    maxResultAsteroid : int = 100
-    numberOfAsteroids : int = 5
-    speed : int = 6    # how many pixels the asteroid will move by each loop, not final
-    yPos : int = 100      # will not be 100, should be the top of screen
-    firstOp : int
-    secondOp : int
-    correctAnswer : int
-
-
 
     # Constructor method
     def __init__(self, mode) -> None:
+        self.maxAsteroid : int = 50
+        self.minAsteroid : int = 1
+        self.maxResultAsteroid : int = 100
+        self.numberOfAsteroids : int = 5
+        self.speed : int = 1    # how many pixels the asteroid will move by each loop, not final
+        self.firstOp : int
+        self.secondOp : int
+        self.correctAnswer : int
 
-        # Generate random operands within specified range
+
+        # Calculate and set the correct answer to the question
+        self.qAnswer = 0
+        # set x position from input
+        # input will be an int from 1 to 5, the saved value will be multiplied by a constant to get actual position
+
+        # Initialize array to store asteroid values
+        self.asteroidArr = []
+        self.question_surface = ''
+
+        # Store the mode of the asteroid (1 for addition, 2 for subtraction, 3 for multiplication)
+        self.mode = mode
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
+        self.incrementsize = 800 / self.numberOfAsteroids
+
+
+        currentPath = os.path.dirname(__file__)  # __file__ is the path to the current script
+        asteroidImagePath = os.path.join(currentPath, "..", "components", "Images", "asteroid.png")
+        self.asteroidImagePath = pygame.image.load(os.path.normpath(asteroidImagePath))
+
+    def create_question(self):
+         # generate answer based on mode
+
         self.firstOp = random.randint(self.minAsteroid, self.maxAsteroid)
         self.secondOp = random.randint(self.minAsteroid, self.maxAsteroid)
 
         # Calculate and set the correct answer to the question
-        self.qAnswer = self.firstOp + self.secondOp   # correct answer to question, this needs to change to reflect mode
-        # set x position from input
-        # input will be an int from 1 to 5, the saved value will be multiplied by a constant to get actual position
-       
-        # Initialize array to store asteroid values
-        self.asteroidArr = []
+        self.qAnswer = self.firstOp + self.secondOp
 
-
-        # Store the mode of the asteroid (1 for addition, 2 for subtraction, 3 for multiplication)
-        self.mode = mode
-
-        # generate answer based on mode
         if self.mode == 1:
             # addition
             self.correctAnswer = self.firstOp + self.secondOp
@@ -47,21 +54,16 @@ class Asteroid():
             self.correctAnswer = self.firstOp - self.secondOp
         else:
             # multiplication
-            self.correctAnswer = self.firstOp * self.secondOp     
-    
-        self.incrementsize = 800 / self.numberOfAsteroids
-
-        currentPath = os.path.dirname(__file__)  # __file__ is the path to the current script
-        asteroidImagePath = os.path.join(currentPath, "..", "components", "Images", "asteroid.png")
-        self.asteroidImagePath = pygame.image.load(os.path.normpath(asteroidImagePath))
-        self.asteroids = []
+            self.correctAnswer = self.firstOp * self.secondOp
 
     def create_asteroids(self, x, y, value, isCorrect):
         # scale it to smaller size and make it quadratic
         surf = pygame.transform.scale(self.asteroidImagePath, (70, 70))
-        
+        number_surface = self.font.render((str(value)), True, (0, 0, 0))
+
         return {
             'surface': surf,
+            'number_surface': number_surface,
             'position': [x, y],
             'value' : value,
             'speed': self.speed,
@@ -78,25 +80,28 @@ class Asteroid():
     def move_asteroids(self):
         for asteroid in self.asteroidArr:
             asteroid['position'][1] += self.speed
-        
+
     def collide_barrer(self):
         pass
 
     # Method to generate asteroids with correct and incorrect answers
     def generateAsteroids(self):
-        
+
         #generate the first correct answer
         #[answer]
         #generate the rest of the incorrect answers
-        # for loop -1 the number of asteroids 
-        # rnd int if not answer and not in asteroid 
+        # for loop -1 the number of asteroids
+        # rnd int if not answer and not in asteroid
         # store value in the array
         # for loop that creates an incorrect answer for each asteroid
-       
-       print(self.correctAnswer)
 
-       picked = random.randint(1, self.numberOfAsteroids) - 1
-       for i in range(0, self.numberOfAsteroids - 1):
+        self.create_question()
+        self.create_question_surface()
+
+        picked = random.randint(0 , self.numberOfAsteroids - 1)
+        print(picked)
+        print(self.correctAnswer)
+        for i in range(0, self.numberOfAsteroids):
             if picked == i:
                 self.asteroidArr.append(self.create_asteroids(self.incrementsize * i, 0, self.correctAnswer, True))
             else:
@@ -106,33 +111,23 @@ class Asteroid():
                     x = random.randint(self.minAsteroid, self.maxResultAsteroid)
                 self.asteroidArr.append(self.create_asteroids(self.incrementsize * i, 0, x, False))
 
-
     def showEquation(self) -> str:
         eq = "%d + %d" % (self.firstOp, self.secondOp)
         return eq
-    
-    def collision(self) -> None:
-        
-        pass
 
-        
-    
+    def create_question_surface(self):
+        self.question_surface = self.font.render(self.showEquation(), True, (0, 0, 0))
+
+
+
 if __name__ == "__main__":
     # Create an instance of the asteroid class
     ass = Asteroid(1)  # mode: 1 (addition) x position: 2
+    ass.create_question()
     print(ass.showEquation())
-    ass.move_asteroids()
-    ass.move_asteroids()
 
-    ass.generateAsteroids()
-    print(ass.asteroidArr)
-    ass.move_asteroids()
-    ass.move_asteroids()
-    ass.move_asteroids()
-    print(ass.asteroidArr)
 
 
     # Generate asteroids with correct and incorrect answers
     # ass.generateAsteroids()
     # Print the array containing asteroid values
-    

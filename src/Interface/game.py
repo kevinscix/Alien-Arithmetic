ac644f33b13d6349bec06599d500df704e41b63d
Andy Jun-Lok Sit <asit5@uwo.ca>
2024-03-31 13:31:48 -0400

    offset added for init

--- src/Interface/game.py
+++ src/Interface/game.py
@@ -210,6 +210,10 @@ class GameState(State):
         self.gamePlay1Image = pygame.transform.scale(self.settings['over'], (800, 600))
         self.user.save_settings(self.user.model_dump_json(), self.user.name)
         
+        #offset for the start
+        self.user.correctAmt -= 1
+        self.user.questionsCompleted -= 1
+        self.user.score -= 10
         #return user to level
 
     def onGameWin(self):
@@ -232,7 +236,11 @@ class GameState(State):
                 self.user.level[1] = 1
         elif self.level < 3:
             self.user.level[1] += 1
-
+        
+        #offset for the start
+        self.user.correctAmt -= 1
+        self.user.questionsCompleted -= 1
+        self.user.score -= 10
         self.user.save_settings(self.user.model_dump_json(), self.user.name)
 
     def on_draw(self, surface):

500bd46ba3fddf796e2e8510337bf0a1c418b2fd
Andy Jun-Lok Sit <asit5@uwo.ca>
2024-03-31 23:02:17 -0400

    no changes

--- src/pydocgen.py
+++ src/pydocgen.py
@@ -6,12 +6,14 @@ Generate the code documentation, using pydoc.
 """
 
 for dirname in ['components', 'Interface']:
+    print(dirname)
     flist = glob.glob(os.path.join(dirname, '*.py'))
+    print(flist)
     for fname in flist:
         print(fname)
         if not fname.endswith('__init__.py'):
-            os.system('python3 -m pydoc -w %s' % fname)
+            os.system('python -m pydoc -w %s' % fname)
             bname = os.path.splitext(os.path.basename(fname))[0]  
             os.system('mv %s.html %s.%s.html' % (bname, dirname, bname))
-    os.system('python3 -m pydoc -w %s' % dirname)
+    os.system('python -m pydoc -w %s' % dirname)
 os.system('mv *.html docs')

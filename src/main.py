3567a1904e4e26b1e36acf8e8187b4c83d5c667c
Kevin Wu <kwu347@uwo.ca>
2024-03-31 20:55:17 -0400

    comments

--- src/main.py
+++ src/main.py
@@ -3,16 +3,24 @@ from Interface.login import LoginState
 import pygame
 import os
 import sys
+# Setup the environment by appending the current directory to the system path.
 current_dir = os.path.dirname(__file__)
 sys.path.append(current_dir)
 
 def main():
-    pygame.init()
+    """
+    The main function that initializes the pygame environment, sets up the game engine, and starts the game loop.
+
+    This function creates an instance of the DisplayEngine, sets the initial state to LoginState, and runs the game loop
+    until the game is exited. Pygame is initialized at the beginning and quit at the end to ensure proper resource management.
+    """
+    pygame.init()# Initialize all imported pygame modules
+    # Create a DisplayEngine object with the specified title, frame rate, and window size
     engine = DisplayEngine('Alien Arithmetic', 60, 800, 600)
+    # Start the game loop with the initial state set to LoginState
     engine.run(LoginState(engine))
 pygame.quit()
 
-print(current_dir)
 main()
 
 
